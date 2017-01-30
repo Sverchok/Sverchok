04c24f3a54e996db03f15132de22ea8a2152146e
zeffii <tetha.z@gmail.com>
2017-01-30 15:13:57 +0100

    commit final entry, untested

--- nodes/number/trig.py
+++ nodes/number/trig.py
@@ -8,28 +8,16 @@ from svrx.typing import Number, Float, Int
 
 local_name = 'SvRxNodeTrig'
 
-'''over view
-
--  SINE
--  COSINE
--  SIN(x), COS(x)
--  DEGREES
--  RADIANS
--  TANGENT
--  ARCSINE
--  ARCCOSINE
--  ARCTANGENT
-  ACOSH
-  ASINH
-  ATANH
-  COSH
-  SINH
-  TANH
+'''
+-  SINE     / COSINE     / SIN(x), COS(x) 
+-  DEGREES  / RADIANS    / TANGENT
+-  ARCSINE  / ARCCOSINE  / ARCTANGENT
+-  ACOSH    / ASINH      / ATANH
+-  COSH     / SINH       / TANH
   ----- divider ----
-  PI * N, where N defaults to 1.0
-  PHI * N
-  TAU * N
-
+-  PI * N, where N defaults to 1.0 or 2.0
+-  PHI * N
+-  TAU * N
 '''
 
 #  Constants wrapper
@@ -39,56 +27,90 @@ def constant(func):
         return np.array([func()])
     return inner
 
+#  Constants wrapper
+def constant_times_n(func):
+    @wraps(func)
+    def inner(*args):
+        return np.array([func(*args)])
+    return inner
+
 
 @node_func(bl_idname=local_name, multi_label="TRIG", id=0)
 def sine(x: Number = 0.0) -> Number:
-    return sin(x)
+    return np.sin(x)
 
 @node_func(bl_idname=local_name, id=1)
 def cosine(x: Number = 0.0) -> Number:
-    return cos(x)
+    return np.cos(x)
 
 @node_func(bl_idname=local_name, id=2)
-def sincos(x: Number = 0.0) -> Number, Number:
-    return sin(s), cos(x)
+def sincos(x: Number = 0.0) -> [Number, Number]:
+    return np.sin(s), np.cos(x)
 
 @node_func(bl_idname=local_name, id=3)
 def degrees(x: Number = 0.0) -> Number:
-    return degrees(s)
+    return np.degrees(s)
 
 @node_func(bl_idname=local_name, id=4)
 def radians(x: Number = 0.0) -> Number:
-    return radians(s)
+    return np.radians(s)
 
 @node_func(bl_idname=local_name, id=20)
 def tangent(x: Number = 0.0) -> Number:
-    return tan(x)
+    return np.tan(x)
 
 @node_func(bl_idname=local_name, id=30)
 def arcsine(x: Number = 0.0) -> Number:
-    return asin(x)
+    return np.asin(x)
+
+@node_func(bl_idname=local_name, id=31)
+def arcosine(x: Number = 0.0) -> Number:
+    return np.acos(x)
+
+@node_func(bl_idname=local_name, id=32)
+def arctangent(x: Number = 0.0) -> Number:
+    return np.atan(x)
 
 @node_func(bl_idname=local_name, id=40)
-def arccosine(x: Number = 0.0) -> Number:
-    return acos(x)
+def asinh(x: Number = 0.0) -> Number:
+    return np.asinh(x)
+
+@node_func(bl_idname=local_name, id=41)
+def acosh(x: Number = 0.0) -> Number:
+    return np.acosh(x)
+
+@node_func(bl_idname=local_name, id=42)
+def atanh(x: Number = 0.0) -> Number:
+    return np.atanh(x)
 
 @node_func(bl_idname=local_name, id=50)
-def arctangent(x: Number = 0.0) -> Number:
-    return atan(x)
+def sinh(x: Number = 0.0) -> Number:
+    return np.sinh(x)
 
+@node_func(bl_idname=local_name, id=51)
+def cosh(x: Number = 0.0) -> Number:
+    return np.cosh(x)
 
-# @node_func(bl_idname=local_name, multi_label="TRIG", id=0)
-# def sine(x: Number = 0.0, y: Number = 0.0) -> Number:
-#     return sin(x)
+@node_func(bl_idname=local_name, id=52)
+def tanh(x: Number = 0.0) -> Number:
+    return np.tanh(x)
 
-
+## Constants times input n
 
 @node_func(bl_idname=local_name, id=60)
-@constant
-def pi() -> Float:
-    return np.pi
+@constant_times_n
+def pi(n: Number = 2.0) -> Number:
+    return np.pi * n
 
 @node_func(bl_idname=local_name, id=61)
-@constant
-def tau() -> Float:
-    return np.pi*2
+@constant_times_n
+def tau(n: Number = 1.0) -> Number:
+    return np.pi * n * 2
+
+@node_func(bl_idname=local_name, id=62)
+@constant_times_n
+def e(n: Number = 1.0) -> Number:
+    return np.e * n
+
+
+
