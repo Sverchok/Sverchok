8a83c0dae70ce054420b7e911704d3d5a0994988
zeffii <tetha.z@gmail.com>
2017-01-30 14:38:09 +0100

    push second part of first commits

--- nodes/number/trig.py
+++ nodes/number/trig.py
@@ -12,41 +12,75 @@ local_name = 'SvRxNodeTrig'
 
 -  SINE
 -  COSINE
+-  SIN(x), COS(x)
+-  DEGREES
+-  RADIANS
 -  TANGENT
 -  ARCSINE
 -  ARCCOSINE
 -  ARCTANGENT
--  DEGREES
--  RADIANS
--  ACOSH
--  ASINH
--  ATANH
--  COSH
--  SINH
--  TANH
--  PI * N, where N defaults to 1.0
--  PHI * N
--  TAU * N
--  SIN(x), COS(x)
+  ACOSH
+  ASINH
+  ATANH
+  COSH
+  SINH
+  TANH
+  ----- divider ----
+  PI * N, where N defaults to 1.0
+  PHI * N
+  TAU * N
 
 '''
 
-
-@node_func(bl_idname=local_name, multi_label="TRIG", id=0)
-def add(x: Number = 0.0, y: Number = 0.0) -> Number:
-    return x + y
-
-@node_func(bl_idname=local_name, id=1)
-def negate(x: Number = 0.0) -> Number:
-    return -x
-
-#  Constants
-
+#  Constants wrapper
 def constant(func):
     @wraps(func)
     def inner():
         return np.array([func()])
     return inner
+
+
+@node_func(bl_idname=local_name, multi_label="TRIG", id=0)
+def sine(x: Number = 0.0) -> Number:
+    return sin(x)
+
+@node_func(bl_idname=local_name, id=1)
+def cosine(x: Number = 0.0) -> Number:
+    return cos(x)
+
+@node_func(bl_idname=local_name, id=2)
+def sincos(x: Number = 0.0) -> Number, Number:
+    return sin(s), cos(x)
+
+@node_func(bl_idname=local_name, id=3)
+def degrees(x: Number = 0.0) -> Number:
+    return degrees(s)
+
+@node_func(bl_idname=local_name, id=4)
+def radians(x: Number = 0.0) -> Number:
+    return radians(s)
+
+@node_func(bl_idname=local_name, id=20)
+def tangent(x: Number = 0.0) -> Number:
+    return tan(x)
+
+@node_func(bl_idname=local_name, id=30)
+def arcsine(x: Number = 0.0) -> Number:
+    return asin(x)
+
+@node_func(bl_idname=local_name, id=40)
+def arccosine(x: Number = 0.0) -> Number:
+    return acos(x)
+
+@node_func(bl_idname=local_name, id=50)
+def arctangent(x: Number = 0.0) -> Number:
+    return atan(x)
+
+
+# @node_func(bl_idname=local_name, multi_label="TRIG", id=0)
+# def sine(x: Number = 0.0, y: Number = 0.0) -> Number:
+#     return sin(x)
+
 
 
 @node_func(bl_idname=local_name, id=60)
