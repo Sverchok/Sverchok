ad75ae24a3c5393b8f3359453ec7a897295bae61
ly29 <linusyng@live.com>
2017-01-30 11:44:55 +0100

    new math node

--- nodes/number/math.py
+++ nodes/number/math.py
@@ -1,25 +1,34 @@
+
+from functools import wraps
+import numpy as np
+
 from svrx.nodes.node_base import node_func
 
-from svrx.typing import Number, Float, Int, EnumP
+from svrx.typing import Number, Float, Int
+
+@node_func(bl_idname='SvRxNodeMath', multi_label="MATH", id=0)
+def add(x: Number = 0.0, y: Number = 0.0) -> Number:
+    return x + y
+
+@node_func(bl_idname='SvRxNodeMath', id=1)
+def negate(x: Number = 0.0) -> Number:
+    return -x
+
+#  Constants
+
+def constant(func):
+    @wraps(func)
+    def inner():
+        return np.array([func()])
+    return inner
 
 
-mode_list_tmp = [('ADD', 1), ('SUB', 2)]
+@node_func(bl_idname='SvRxNodeMath', id=60)
+@constant
+def pi() -> Float:
+    return np.pi
 
-mode_list = [(t, t, t, idx) for t, idx in mode_list_tmp]
-
-
-@node_func(bl_idname='SvRxNodeNumberMath')
-def math(x: Number = 0.0, y: Number = 0.0,
-        mode: EnumP(items=mode_list, default='ADD') = 'ADD')-> Number:
-    f = func_lookup[mode]
-    return f(x, y)
-
-
-def add(x, y):
-    return x  + y
-
-def sub(x, y):
-    return x - y
-
-func_lookup = {'ADD': add,
-               'SUB': sub, }
+@node_func(bl_idname='SvRxNodeMath', id=61)
+@constant
+def e() -> Float:
+    return np.e
