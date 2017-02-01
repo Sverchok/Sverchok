60d303c1e6904222d9678ebe6b3b44dfb3aa8594
zeffii <tetha.z@gmail.com>
2017-02-01 19:59:03 +0100

    added more vmath modes

--- nodes/vertex/vector_math.py
+++ nodes/vertex/vector_math.py
@@ -1,72 +1,79 @@
 import numpy as np
 
-from svrx.typing import Vertices, Float
+from svrx.typing import Vector, Float, Int, Vertices
+from svrx.nodes.node_base import node_func
+
 from svrx.util.function import make_compatible
 
-# pylint: disable=C0326
-null_vector = (0.0, 0.0, 0.0)
+X_AXIS = (1, 0, 0)
+Y_AXIS = (0, 1, 0)
+Z_AXIS = (0, 0, 1)
+ZEROS = (0, 0, 0)
 
-def add(u: Vertices = null_vector, v: Vertices = null_vector ) -> Vertices:
+# pylint: disable=C0326
+
+@node_func(bl_idname="SvRxVectorMathNode", multi_label="Vector Math", id=0)
+def add(u: Vertices = ZEROS, v: Vertices = ZEROS ) -> Vertices:
     u, v = make_compatible(u, v)
     return u + v
 
-
-def sub(u: Vertices = null_vector, v: Vertices = null_vector ) -> Vertices:
+@node_func(bl_idname="SvRxVectorMathNode", id=6)
+def sub(u: Vertices = ZEROS, v: Vertices = ZEROS ) -> Vertices:
     u, v = make_compatible(u, v)
     return u - v
 
-
-def cross(u: Vertices = null_vector, v: Vertices = null_vector ) -> Vertices:
+@node_func(bl_idname="SvRxVectorMathNode", id=12)
+def cross(u: Vertices = ZEROS, v: Vertices = ZEROS ) -> Vertices:
     u, v = make_compatible(u, v)
     return np.cross(u, v)
 
-
-def scale(u: Vertices = null_vector, s: Float = 1.0 ) -> Vertices:
+@node_func(bl_idname="SvRxVectorMathNode", id=18)
+def scale(u: Vertices = ZEROS, s: Float = 1.0 ) -> Vertices:
     u, s = make_compatible(u, s)
     return u * s
 
-
-def scale_reciprocal(u: Vertices = null_vector, s: Float = 1.0 ) -> Vertices:
+@node_func(bl_idname="SvRxVectorMathNode", id=22)
+def scale_reciprocal(u: Vertices = ZEROS, s: Float = 1.0 ) -> Vertices:
     u, s = make_compatible(u, s)
     return u * (1 / s)
 
-
-def length(u: Vertices = null_vector ) -> Float:
+@node_func(bl_idname="SvRxVectorMathNode", id=26)
+def length(u: Vertices = ZEROS ) -> Float:
     return np.linalg.norm(u, axis=0)
 
-
-def dot(u: Vertices = null_vector, v: Vertices = null_vector ) -> Float:
+@node_func(bl_idname="SvRxVectorMathNode", id=32)
+def dot(u: Vertices = ZEROS, v: Vertices = ZEROS ) -> Float:
     return u.dot(v)  # .. not sure
 
-
-def opposite(u: Vertices = null_vector ) -> Vertices:
+@node_func(bl_idname="SvRxVectorMathNode", id=38)
+def opposite(u: Vertices = ZEROS ) -> Vertices:
     return -u
 
-
-def distance(u: Vertices = null_vector, v: Vertices = null_vector ) -> Float:
+@node_func(bl_idname="SvRxVectorMathNode", id=44)
+def distance(u: Vertices = ZEROS, v: Vertices = ZEROS ) -> Float:
     # speed!?  http://stackoverflow.com/a/9184560/1243487
     u, v = make_compatible(u, v)
     x = u - v
     return np.sqrt(x * x)
 
-
-def rx_round(u: Vertices = null_vector, n: Int = 7 ) -> Vertices:
+@node_func(bl_idname="SvRxVectorMathNode", multi_label="round", id=48)
+def rx_round(u: Vertices = ZEROS, n: Int = 7 ) -> Vertices:
     return np.round_(u, n)
 
 
 
-func_list = {
-    "Cross":    (1, cross),
-    "Add":      (4, add),
-    "Sub":      (8, sub),
-    "Scale":    (12, scale),
-    "1/Scale":  (16, scale_reciprocal),    
-    "Length":   (20, length),
-    "Dot":      (24, dot),
-    "Opposite": (30, opposite),
-    "Distance": (40, distance),
-    "Round":    (50, rx_round)
-}
+# func_list = {
+#     "Cross":    (1, cross),
+#     "Add":      (4, add),
+#     "Sub":      (8, sub),
+#     "Scale":    (12, scale),
+#     "1/Scale":  (16, scale_reciprocal),    
+#     "Length":   (20, length),
+#     "Dot":      (24, dot),
+#     "Opposite": (30, opposite),
+#     "Distance": (40, distance),
+#     "Round":    (50, rx_round)
+# }
 
 """
 class SvrxVectorMathNode:
