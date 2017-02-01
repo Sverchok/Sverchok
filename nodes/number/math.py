cdec70f87975a24fbc25212a31a9b90e1769f67b
zeffii <tetha.z@gmail.com>
2017-02-01 22:19:22 +0100

    modes to math node

--- nodes/number/math.py
+++ nodes/number/math.py
@@ -6,68 +6,75 @@ from svrx.nodes.classes import NodeMathBase
 from svrx.typing import Number, Float, Int
 from svrx.util.function import constant
 
-@node_func(bl_idname='SvRxNodeMath', multi_label="Math", id=0, cls_bases=(NodeMathBase,))
+# pylint: disable=C0326
+
+MATHNODE = 'SvRxNodeMath'
+
+@node_func(bl_idname=MATHNODE, multi_label="Math", id=0, cls_bases=(NodeMathBase,))
 def add(x: Number = 0.0, y: Number = 0.0) -> Number:
     return x + y
 
-@node_func(bl_idname='SvRxNodeMath', id=1)
+@node_func(bl_idname=MATHNODE, id=1)
 def sub(x: Number = 0.0, y: Number = 0.0) -> Number:
     return x - y
 
-@node_func(bl_idname='SvRxNodeMath', id=2)
+@node_func(bl_idname=MATHNODE, id=2)
 def mul(x: Number = 0.0, y: Number = 0.0) -> Number:
     return x * y
 
-@node_func(bl_idname='SvRxNodeMath', id=3)
+@node_func(bl_idname=MATHNODE, id=3)
 def div(x: Number = 0.0, y: Number = 0.0) -> Number:
     return x / y
 
-@node_func(bl_idname='SvRxNodeMath', id=4)
+@node_func(bl_idname=MATHNODE, id=4)
 def sqrt(x: Number = 1.0) -> Number:
     return x ** .5
 
-@node_func(bl_idname='SvRxNodeMath', id=5)
+@node_func(bl_idname=MATHNODE, id=5)
 def copy_sign(x: Number = 1.0, y: Number = 0.0) -> Number:
     return np.copy_sign(x, y)
 
 
+@node_func(bl_idname=MATHNODE, id=6)
+def abs(x: Number = 1.0) -> Number:
+    return np.absolute(x)
 
-@node_func(bl_idname='SvRxNodeMath', id=10)
+
+@node_func(bl_idname=MATHNODE, id=9)
+def reciprocal(x: Number = 0.0) -> Number:
+    return 1 / x
+
+@node_func(bl_idname=MATHNODE, id=10)
 def negate(x: Number = 0.0) -> Number:
     return -x
 
-@node_func(bl_idname='SvRxNodeMath', id=11)
+@node_func(bl_idname=MATHNODE, id=11)
 def add_1(x: Number = 0.0) -> Number:
     return x + 1
 
-@node_func(bl_idname='SvRxNodeMath', id=12)
+@node_func(bl_idname=MATHNODE, id=12)
 def sub_1(x: Number = 0.0) -> Number:
     return x - 1
 
-@node_func(bl_idname='SvRxNodeMath', id=13)
+@node_func(bl_idname=MATHNODE, id=13)
 def div_2(x: Number = 0.0) -> Number:
     return x / 2
 
-@node_func(bl_idname='SvRxNodeMath', id=14)
+@node_func(bl_idname=MATHNODE, id=14)
 def mul_2(x: Number = 0.0) -> Number:
     return x * 2
 
-@node_func(bl_idname='SvRxNodeMath', id=15)
+
+@node_func(bl_idname=MATHNODE, id=15)
 def as_int(x: Number = 0.0) -> Int:
     return x.astype(int)
 
-@node_func(bl_idname='SvRxNodeMath', id=16)
+@node_func(bl_idname=MATHNODE, id=16)
 def round(x: Number = 0.0, y: Int = 0) -> Float:
     return x.round(y)
 
 
-
-@node_func(bl_idname='SvRxNodeMath', id=60)
-@constant
-def pi() -> Float:
-    return np.pi
-
-@node_func(bl_idname='SvRxNodeMath', id=61)
+@node_func(bl_idname=MATHNODE, id=61)
 @constant
 def e() -> Float:
     return np.e
