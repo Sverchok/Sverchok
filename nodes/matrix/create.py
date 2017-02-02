d1b7f175ed55aee8a71589223416a30d0fd27fed
ly29 <linusyng@live.com>
2017-02-02 10:51:19 +0100

    matrix create working

--- nodes/matrix/create.py
+++ nodes/matrix/create.py
@@ -1,12 +1,20 @@
 import numpy as np
 from svrx.nodes.node_base import node_func
-from svrx.util.transforms import translation_matrix
+from svrx.util.transforms import translation_matrix, concatenate_matrices, rotation_matrix, scale_matrix
 from svrx.util.geom import vectorize
 
 from svrx.typing import Vector, Float, Matrix, List
 
 
-trans_mat = vectorize(translation_matrix)
+@vectorize
+def mat_create(trans, scale, rotation, angle):
+    t = translation_matrix(trans)
+    s = np.identity(4)
+    for i in range(3):
+        s[i,i] = scale[i]
+    r = rotation_matrix(np.radians(angle), rotation)
+    mat = concatenate_matrices(t, s, r)
+    return mat
 
 
 @node_func(bl_idname="SvRxNodeCreateMatrix")
@@ -16,4 +24,4 @@ def create_matrix(location: Vector = (0.0, 0.0, 0.0),
                   angle: Float = 0.0
                   ) -> [Matrix]:
 
-    return list(trans_mat(location))
+    return list(mat_create(location, scale, rotation, angle))
