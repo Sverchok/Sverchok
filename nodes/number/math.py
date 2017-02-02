88465144b0262b454badb4803b28d5bc8edecb9a
ly29 <linusyng@live.com>
2017-02-02 08:17:08 +0100

    lighten math

--- nodes/number/math.py
+++ nodes/number/math.py
@@ -10,64 +10,64 @@ from svrx.util.function import constant
 def add(x: Number = 0.0, y: Number = 0.0) -> Number:
     return x + y
 
-@node_func(bl_idname='SvRxNodeMath', id=1)
+@node_func(id=1)
 def sub(x: Number = 0.0, y: Number = 0.0) -> Number:
     return x - y
 
-@node_func(bl_idname='SvRxNodeMath', id=2)
+@node_func(id=2)
 def mul(x: Number = 0.0, y: Number = 0.0) -> Number:
     return x * y
 
-@node_func(bl_idname='SvRxNodeMath', id=3)
+@node_func(id=3)
 def div(x: Number = 0.0, y: Number = 0.0) -> Number:
     return x / y
 
-@node_func(bl_idname='SvRxNodeMath', id=4)
+@node_func(id=4)
 def sqrt(x: Number = 1.0) -> Number:
-    return x ** .5
+    return np.sqrt(x)
 
-@node_func(bl_idname='SvRxNodeMath', id=5)
+@node_func(id=5)
 def copy_sign(x: Number = 1.0, y: Number = 0.0) -> Number:
     return np.copy_sign(x, y)
 
 
 
-@node_func(bl_idname='SvRxNodeMath', id=10)
+@node_func(id=10)
 def negate(x: Number = 0.0) -> Number:
     return -x
 
-@node_func(bl_idname='SvRxNodeMath', id=11)
+@node_func(id=11)
 def add_1(x: Number = 0.0) -> Number:
     return x + 1
 
-@node_func(bl_idname='SvRxNodeMath', id=12)
+@node_func(id=12)
 def sub_1(x: Number = 0.0) -> Number:
     return x - 1
 
-@node_func(bl_idname='SvRxNodeMath', id=13)
+@node_func(id=13)
 def div_2(x: Number = 0.0) -> Number:
     return x / 2
 
-@node_func(bl_idname='SvRxNodeMath', id=14)
+@node_func(id=14)
 def mul_2(x: Number = 0.0) -> Number:
     return x * 2
 
-@node_func(bl_idname='SvRxNodeMath', id=15)
+@node_func(id=15)
 def as_int(x: Number = 0.0) -> Int:
     return x.astype(int)
 
-@node_func(bl_idname='SvRxNodeMath', id=16)
+@node_func(id=16)
 def round(x: Number = 0.0, y: Int = 0) -> Float:
     return x.round(y)
 
 
 
-@node_func(bl_idname='SvRxNodeMath', id=60)
+@node_func(id=60)
 @constant
 def pi() -> Float:
     return np.pi
 
-@node_func(bl_idname='SvRxNodeMath', id=61)
+@node_func(id=61)
 @constant
 def e() -> Float:
     return np.e
