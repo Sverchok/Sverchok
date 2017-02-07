1a4151f3843a61273a0441ee0059e5189c60dca6
zeffii <tetha.z@gmail.com>
2017-02-07 15:04:10 +0100

    subtract mode light pass

--- nodes/vertex/vector_math.py
+++ nodes/vertex/vector_math.py
@@ -34,7 +34,11 @@ def add(u: Vertices = ZEROS, v: Vertices = ZEROS ) -> Vertices:
 @node_func(id=6)
 def sub(u: Vertices = ZEROS, v: Vertices = ZEROS ) -> Vertices:
     u, v = make_compatible(u, v)
-    return u - v
+    max_len, _ = np.shape(u)
+    out = np.zeros((max_len, 4))
+    out[:,:3] = u[:,:3] - v[:,:3]
+    out[:, 3] = min(max(u[:,3], v[:,3]), 1.0)    
+    return out
 
 @node_func(id=12)
 def cross(u: Vertices = ZEROS, v: Vertices = ZEROS ) -> Vertices:
