ff4768d6311aacda13afb43d3f82634f04d552fa
zeffii <tetha.z@gmail.com>
2017-02-08 21:16:02 +0100

    ditch the length test

--- util/mesh.py
+++ util/mesh.py
@@ -37,14 +37,14 @@ def bmesh_from_pydata(verts, edges=None, faces=None, normal_update=False):
     bm.verts.index_update()
     bm.verts.ensure_lookup_table()
 
-    if not faces is None and len(faces):
+    if not faces is None:
         add_face = bm.faces.new
         for face in faces:
             add_face(tuple(bm.verts[i] for i in face))
 
         bm.faces.index_update()
 
-    if not edges is None and len(edges):
+    if not edges is None:
         add_edge = bm.edges.new
         for edge in edges:
             edge_seq = tuple(bm.verts[i] for i in edge)
@@ -73,7 +73,6 @@ def rxdata_from_bm(bm):
 def rxdata_from_pydata(verts, edges=None, faces=None):
     # return SMesh.from_pydata(verts, edges, faces)
     v = SvVertices.from_pydata(verts)
-    E = SvEdges(edges)
-    e = E.from_pydata(edges)
+    e = SvEdges.from_pydata(edges)
     f = SvPolygon.from_pydata(faces)
     return v, e, f
