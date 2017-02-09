ccd3cfc526adb46b9d44e857db4355ed236e174f
ly29 <linusyng@live.com>
2017-02-09 14:35:28 +0100

    bmesh nodes

---
+++ nodes/bmesh/generators.py
@@ -0,0 +1,30 @@
+import bmesh
+
+from svrx.typing import Float, Int, Vector, BMesh
+from svrx.nodes.node_base import node_func
+from svrx.util.geom import generator
+
+@node_func(bl_idname='SvRxNodeBmeshGenerate', multi_label="Bmesh Gen", id=0)
+def create_monkey() -> BMesh:
+    bm = bmesh.new()
+    bmesh.ops.create_monkey(bm)
+    return bm
+
+
+def make_icosphere(subdiv, diam):
+    bm = bmesh.new()
+    bmesh.ops.create_icosphere(bm, subdivisions=subdiv, diameter=diam, calc_uvs=False)
+    return bm
+
+@node_func(id=1)
+@generator
+def create_icosphere(subdiv: Int(min=0, max=8) = 2, diam: Float = 1.0) -> BMesh:
+    return make_icosphere(min(subdiv, 8), diam)
+
+
+@node_func(id=2)
+@generator
+def create_cube(size: Float = 1.0) -> BMesh:
+    bm = bmesh.new()
+    bmesh.ops.create_cube(bm, size=size)
+    return bm
---
+++ nodes/bmesh/modifiers.py
@@ -0,0 +1,37 @@
+import bmesh
+
+from svrx.typing import Float, Int, Vector, BMesh, Required
+from svrx.nodes.node_base import node_func
+from svrx.util.geom import generator
+
+
+@node_func(bl_idname='SvRxNodeBmeshModifiers', multi_label='Bmesh modifiers', id=0)
+def wireframe(bm: BMesh = Required, t: Float = 0.01) -> BMesh:
+    bm = bm.copy()
+    bmesh.ops.wireframe(
+        bm, faces=bm.faces[:],
+        thickness=t,
+        offset=True,
+        use_replace=True,
+        use_boundary=True,
+        use_even_offset=True,
+        use_relative_offset=True)
+    return bm
+
+@node_func(id=2)
+def recalc_normals(bm: BMesh = Required) -> BMesh:
+    bm = bm.copy()
+    bmesh.ops.recalc_face_normals(bm, faces=bm.faces[:])
+
+@node_func(id=3)
+def update_normals(bm: BMesh = Required) -> BMesh:
+    bm = bm.copy()
+    bm.normal_update()
+    return bm
+
+@node_func(id=1)
+def solidify(bm: BMesh = Required, thickness: Float = 1.0) -> BMesh:
+    bm = bm.copy()
+    geom_in = bm.verts[:] + bm.edges[:] + bm.faces[:]
+    bmesh.ops.solidify(bm, geom_in, thickness)
+    return bm
