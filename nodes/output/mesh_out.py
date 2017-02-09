32e12b946c278843dabb26915f0de2a9bcf3effb
ly29 <linusyng@live.com>
2017-02-09 15:15:44 +0100

    mesh out

--- nodes/output/mesh_out.py
+++ nodes/output/mesh_out.py
@@ -2,59 +2,17 @@ import bpy
 import bmesh
 
 from svrx.nodes.node_base import stateful
-from svrx.typing import Vertices, Required, Faces, Edges, StringP, IntP, Matrix
+from svrx.typing import Vertices, Required, Faces, Edges, StringP, IntP, Matrix, BMesh
 from svrx.util.mesh import bmesh_from_pydata
 
 # pylint: disable=C0326
 
-@stateful
-class MeshOut():
-
+class Mesh_out_common:
     def __init__(self, node=None):
         if node:
             self.base_name = node.mesh_name
             self.max_mesh_count = node.max_mesh_count
         self.start()
-
-    bl_idname = "SvRxNodeMeshOut"
-    label = "Mesh out"
-
-    properties = {'mesh_name': StringP(name='Mesh name', default="svrx_mesh"),
-                  'max_mesh_count': IntP(name="Max count", default=100)}
-
-    def start(self):
-        self.verts = []
-        self.edges = []
-        self.faces = []
-        self.mats = []
-
-    def stop(self):
-        obj_index = 0
-        #  using range to limit object number for now during testing
-        param = zip(range(self.max_mesh_count), self.verts, self.edges, self.faces, self.mats)
-        for idx, verts, edges, faces, mat in param:
-            obj_index = idx
-            obj = make_bmesh_geometry(verts[:, :3], edges, faces,
-                                      name=self.base_name,
-                                      idx=idx,
-                                      normal_update=False)
-            if mat is not None:
-                obj.matrix_world = mat.T
-
-        # cleanup
-        self.remove_non_updated_objects(obj_index)
-
-
-    def __call__(self,
-                 verts: Vertices = Required,
-                 edges: Edges = None,
-                 faces: Faces = None,
-                 matrix: Matrix = None):
-        self.verts.append(verts)
-        self.edges.append(edges)
-        self.faces.append(faces)
-        self.mats.append(matrix)
-
 
     def get_children(self, basename):
         objects = bpy.data.objects
@@ -83,9 +41,79 @@ class MeshOut():
         for object_name in objs:
             meshes.remove(meshes[object_name])
 
+@stateful
+class MeshOut(Mesh_out_common):
+
+    bl_idname = "SvRxNodeMeshOut"
+    label = "Mesh out"
+
+    properties = {'mesh_name': StringP(name='Mesh name', default="svrx_mesh"),
+                  'max_mesh_count': IntP(name="Max count", default=100)}
+
+    def start(self):
+        self.verts = []
+        self.edges = []
+        self.faces = []
+        self.mats = []
+
+    def stop(self):
+        obj_index = 0
+        #  using range to limit object number for now during testing
+        param = zip(range(self.max_mesh_count), self.verts, self.edges, self.faces, self.mats)
+        for idx, verts, edges, faces, mat in param:
+            obj_index = idx
+            bm = bmesh_from_pydata(verts[:, :3], edges, faces, normal_update=False)
+            obj = make_bmesh_geometry(bm,
+                                      name=self.base_name,
+                                      idx=idx,)
+            if mat is not None:
+                obj.matrix_world = mat.T
+
+        # cleanup
+        self.remove_non_updated_objects(obj_index)
 
 
-def make_bmesh_geometry(verts, edges=None, faces=None, name="svrx_mesh", idx=0, normal_update=True):
+    def __call__(self,
+                 verts: Vertices = Required,
+                 edges: Edges = None,
+                 faces: Faces = None,
+                 matrix: Matrix = None):
+        self.verts.append(verts)
+        self.edges.append(edges)
+        self.faces.append(faces)
+        self.mats.append(matrix)
+
+
+
+
+
+@stateful
+class BMesh_out(Mesh_out_common):
+    bl_idname = "SvRxNodeBmeshOut"
+    label = "BMesh out"
+
+    properties = {'mesh_name': StringP(name='Mesh name', default="svrx_bm"),
+                  'max_mesh_count': IntP(name="Max count", default=100)}
+    def start(self):
+        self.meshes = []
+        self.mats = []
+
+    def __call__(self, bm: BMesh = Required, mat: Matrix = None):
+        self.meshes.append(bm.copy())
+        self.mats.append(mat)
+
+    def stop(self):
+        obj_index = 0
+        for idx, bm, mat in zip(range(self.max_mesh_count), self.meshes, self.mats):
+            obj_index = idx
+            obj = make_bmesh_geometry(bm, name=self.base_name, idx=idx)
+            if mat is not None:
+                obj.matrix_world = mat.T
+
+        self.remove_non_updated_objects(obj_index)
+
+
+def make_bmesh_geometry(bm, name="svrx_mesh", idx=0):
 
     scene = bpy.context.scene
     meshes = bpy.data.meshes
@@ -107,7 +135,6 @@ def make_bmesh_geometry(verts, edges=None, faces=None, name="svrx_mesh", idx=0, 
     # at this point the mesh is always fresh and empty
 
     ''' get bmesh, write bmesh to obj, free bmesh'''
-    bm = bmesh_from_pydata(verts, edges, faces, normal_update)
     bm.to_mesh(obj.data)
     bm.free()
 
