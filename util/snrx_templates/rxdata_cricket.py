38c829ec0b211105abb4b7b91b2d457f8dfbfe14
zeffii <tetha.z@gmail.com>
2017-02-11 20:05:14 +0100

    drop py ext is needed for now

--- util/snrx_templates/rxdata_cricket.py
+++
@@ -1,8 +0,0 @@
-from svrx.util.mesh import rxdata_from_pydata
-from svrx.util.importers import named_mesh_path, obj_to_pydata_lite
-
-@node_script
-def sn_make_cricket() -> (Vertices, Edges, Faces):
-    obj_path = named_mesh_path("cricket.obj")       #  returns the library path +  object name
-    verts, _, faces = obj_to_pydata_lite(obj_path)  #  accepts full valid path
-    return rxdata_from_pydata(verts, faces=faces)
