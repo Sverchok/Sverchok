fc3f6ecd337a531b196deefea048070a3411123d
ly29 <linusyng@live.com>
2017-02-16 09:36:25 +0100

    rxdata out

--- nodes/output/mesh_out.py
+++ nodes/output/mesh_out.py
@@ -1,9 +1,12 @@
+import numpy as np
+
 import bpy
 import bmesh
 
 from svrx.nodes.node_base import stateful
 from svrx.typing import Vertices, Required, Faces, Edges, StringP, IntP, Matrix, BMesh
 from svrx.util.mesh import bmesh_from_pydata, rxdata_to_mesh
+
 
 # pylint: disable=C0326
 
@@ -17,7 +20,7 @@ class Mesh_out_common:
 
     def get_children(self, basename, kind='MESH'):
         """
-        This finds those objects that are associated with the basename provided by 
+        This finds those objects that are associated with the basename provided by
         the node's interface. kind can be MESH / CURVE
         """
         objects = bpy.data.objects
@@ -28,7 +31,7 @@ class Mesh_out_common:
     def remove_non_updated_objects(self, obj_index, kind='MESH'):
         """
         This function removes those objects that the node no longer is asked to
-        update. This is necessary because a node can be asked to generate any 
+        update. This is necessary because a node can be asked to generate any
         number of objects, and that number can fluctuate on consecutive updates.
 
         """
@@ -149,9 +152,14 @@ class RxMeshOut(Mesh_out_common):
         #  using range to limit object number for now during testing
         param = zip(range(self.max_mesh_count), self.verts, self.edges, self.faces, self.mats)
         for idx, verts, edges, faces, mat in param:
-            
+
             obj_index = idx
-            obj = update_mesh_geometry(rxdata, name=self.base_name, idx=idx)
+
+            obj = get_obj_for(self.base_name, obj_index)
+            res = write_to_mesh(obj, verts, edges, faces)
+            obj.update_tag(refresh={'OBJECT', 'DATA'})
+            obj.hide_select = False
+
             if mat is not None:
                 obj.matrix_world = mat.T
 
@@ -226,5 +234,80 @@ def update_mesh_geometry(rxdata, name="svrx_mesh", idx=0):
     obj.hide_select = False
     return obj
 
+def get_obj_for(name="svrx", idx=0):
+    scene = bpy.context.scene
+    meshes = bpy.data.meshes
+    objects = bpy.data.objects
 
+    rx_name = name + "." + str(idx).zfill(4)
 
+    if rx_name in objects:
+        obj = objects[rx_name]
+    else:
+        # this is only executed once, upon the first run.
+        mesh = meshes.new(rx_name)
+        obj = objects.new(rx_name, mesh)
+        scene.objects.link(obj)
+
+        obj['idx'] = idx
+        obj['basename'] = name
+
+    # at this point the mesh is always fresh and empty
+
+    return obj
+
+def write_to_mesh(obj, vertices, edges=None, faces=None):
+    mesh = obj.data
+    vert_diff = len(vertices) - len(mesh.vertices)
+    if edges is not None and faces is None:
+        edge_diff = len(edges) - len(mesh.edges)
+    elif faces is not None:
+        #   we have faces, edges will be updated using calc_edges
+        edge_diff = 0
+    else: #  no incoming faces or edges
+        edge_diff = - len(mesh.edges)
+
+    if faces is not None:
+        face_diff = len(faces) - len(mesh.polygons)
+        loop_diff = len(faces.vertex_indices) - len(mesh.loops)
+    else:
+        face_diff = -len(mesh.polygons)
+        loop_diff = -len(mesh.loops)
+    diffs = (vert_diff, edge_diff, face_diff, loop_diff)
+
+    if any(n < 0 for n in diffs):
+        print("giving up fast path, bmesh it is", diffs)
+        bm = bmesh_from_pydata(vertices[:, :3].tolist(), edges, faces, normal_update=False)
+        bm.to_mesh(mesh)
+        return True
+    elif any(n > 0 for n in diffs):
+        print("adding vertices")
+        mesh.vertices.add(vert_diff)
+        mesh.edges.add(edge_diff)
+        mesh.loops.add(loop_diff)
+        mesh.polygons.add(face_diff)
+
+    vert_out = vertices[:,:3].astype(np.float32)
+    vert_out.shape = (3 * len(vert_out))
+    mesh.vertices.foreach_set('co', vert_out)
+
+    if edges is not None:
+        edges.shape = (2 * len(edges))
+        mesh.edges.foreach_set('vertices', edges)
+
+    if faces is not None:
+        mesh.polygons.foreach_set("loop_total", faces.loop_info[:,0].copy())
+        mesh.polygons.foreach_set("loop_start", faces.loop_info[:,1].copy())
+        mesh.loops.foreach_set("vertex_index", faces.vertex_indices)
+
+    if faces is not None and edges is None:
+        print("updating edges")
+        mesh.update(calc_edges=True)
+
+    fail = mesh.validate(verbose=True)
+    if not fail:
+        print("managed to update mesh for: ", obj.name)
+    else:
+        print("horrible failue for mesh for: ", obj.name)
+
+    return fail
