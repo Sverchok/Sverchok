9c0b099b9d7196a38a595aa9930f4fe167613de3
ly29 <linusyng@live.com>
2017-02-17 12:36:02 +0100

    torus work / np topology

--- nodes/generator/topology.py
+++ nodes/generator/topology.py
@@ -16,8 +16,9 @@ def line(verts: Vertices = Required) -> (Vertices, Edges, Faces):
 def cirle(verts: Vertices = Required) -> (Vertices, Edges, Faces):
     count = len(verts)
     edges = np.array([np.arange(0, count), np.arange(1, count + 1) % count], dtype=np.uint32).T
-    faces = SvPolygon.from_pydata([np.arange(0, count)])
-
+    faces = SvPolygon(np.array([0], dtype=np.uint32),
+                      np.array([nverts], dtype=np.uint32),
+                      np.arange(0, nverts, dtype=np.uint32))
     return verts, edges, faces
 
 
@@ -26,8 +27,8 @@ def plane(verts: [Vertices] = Required) -> (Vertices, Edges, Faces):
     vertices = np.concatenate(verts)
     y = len(verts)
     x = len(verts[0])
-    edges = np.array(plane_edges(x,     y), dtype=np.uint32)
-    faces = SvPolygon.from_pydata(plane_faces(x, y))
+    edges = pln_edges(x, y)
+    faces = pln_faces(x, y)
     return vertices, edges, faces
 
 
@@ -79,8 +80,27 @@ def plane_faces(x, y):
     for i in range(x-1):
         for j in range(y-1):
             polygons.append((x*j+i, x*j+i+1, x*j+i+x+1, x*j+i+x))
-
     return polygons
+
+def pln_edges(x, y):
+    edges = np.empty((x * (y - 1) + (x - 1) * y, 2 ), dtype=uint32)
+    edges[:(x - 1) * y:, 0] = (np.arange(0, x-1) + np.arange(0, x * y - y, x)[:, np.newaxis]).flatten()
+    edges[(x - 1) * y: 0] = (np.arange(0, x*(y-1), x) + np.arange(0, x)[:,np.newaxis]).flatten()
+    edges[:(x - 1) * y:, 1] = edges[:(x - 1) * y:, 0] + 1
+    edges[(x - 1) * y: 1] = edges[(x - 1) * y: 0] + x
+    return edges
+
+def pln_faces(x, y):
+    faces = np.empty(((x - 1) * (y - 1), 4), dtype=np.uint32)
+    faces[:, 3] = (np.arange(y, x * y, y) + np.arange(0 , y - 1)[:,np.newaxis]).flatten()
+    faces[:, 2] = faces[:, 3] + 1
+    faces[:, 0] = (np.arange(0, x * y -y, y) + np.arange(0, y - 1)[:,np.newaxis]).flatten()
+    faces[:, 1] = faces[:, 0] + 1
+    faces.shape = -1
+    l_total = np.empty((x - 1) * (y - 1), dtype=np.uint32)
+    l_total[:] = 4
+    l_start = np.arange(0, (x - 1) * (y - 1) * 4, 4, dtype=np.uint32)
+    return SvPolygon(l_start, l_total, faces)
 
 
 def cyl_edges(x, y):
@@ -109,3 +129,68 @@ def cyl_faces(x, y, cap=False):
     l_start = np.arange(0, (x * y - y) * 4, 4, dtype=np.uint32)
     p.shape = (x * y -y ) * 4
     return SvPolygon(l_start, l_total, p)
+
+
+
+
+def torus_edges(N1, N2):
+    '''
+
+        N1 : major sections - number of revolution sections around the torus center
+        N2 : minor sections - number of spin sections around the torus tube
+
+    '''
+    listEdges = []
+
+    # spin loop EDGES : around the torus tube
+
+    for n1 in range(N1):
+        for n2 in range(N2-1):
+            listEdges.append([N2*n1 + n2, N2*n1 + n2+1])
+        listEdges.append([N2*n1 + N2-1, N2*n1 + 0])
+
+    # revolution loop EDGES : around the torus center
+
+    for n1 in range(N1-1):
+        for n2 in range(N2):
+            listEdges.append([N2*n1 + n2, N2*(n1+1) + n2])
+    for n2 in range(N2):
+        listEdges.append([N2*(N1-1) + n2, N2*0 + n2])
+
+    return listEdges
+
+
+def torus_faces(x, y):
+    faces = np.empty((x * y, 4), dtype=np.uint32)
+    tmp = np.arange(0, x * y)
+    faces[:, 0] = tmp
+    faces[:, 1] = np.roll(tmp, -y)
+    tmp += 1
+    tmp.shape = (x, y)
+    tmp[:, y - 1] -= y
+    tmp.shape = -1
+    faces[:, 3] = tmp
+    faces[:, 2] = np.roll(tmp, -y)
+    faces.shape = -1
+    l_total = np.empty(x * y, dtype=np.uint32)
+    l_total[:] = 4
+    l_start = np.arange(0, (x * y) * 4, 4, dtype=np.uint32)
+    return SvPolygon(l_start, l_total, faces)
+
+
+
+def torus_polygons(N1, N2):
+    '''
+        N1 : major sections - number of revolution sections around the torus center
+        N2 : minor sections - number of spin sections around the torus tube
+
+    '''
+    listPolys = []
+    for n1 in range(N1-1):
+        for n2 in range(N2-1):
+            listPolys.append([N2*n1 + n2, N2*(n1+1) + n2, N2*(n1+1) + n2+1, N2*n1 + n2+1])
+        listPolys.append([N2*n1 + N2-1, N2*(n1+1) + N2-1, N2*(n1+1) + 0, N2*n1 + 0])
+    for n2 in range(N2-1):
+        listPolys.append([N2*(N1-1) + n2, N2*0 + n2, N2*0 + n2+1, N2*(N1-1) + n2+1])
+    listPolys.append([N2*(N1-1) + N2-1, N2*0 + N2-1, N2*0 + 0, N2*(N1-1) + 0])
+    return listPolys
