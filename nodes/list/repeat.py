0637b64024e4c7a9a56cfd646c4441f0997be905
ly29 <linusyng@live.com>
2017-02-20 10:30:23 +0100

    fix signature repeat/file

--- nodes/list/repeat.py
+++ nodes/list/repeat.py
@@ -2,15 +2,15 @@ import numpy as np
 from svrx.nodes.classes import NodeMathBase
 from svrx.nodes.node_base import node_func
 from svrx.typing import Number, Int
-
+from svrx.util.geom import generator
 
 @node_func(bl_idname='SvRxListRepeat', multi_label="Repeat", id=0, cls_bases=(NodeMathBase,))
-def np_repeat(Values: Number = 0.0, Repeat: Int = 2) -> Number:
+@generator
+def np_repeat(Values: Number(iterable=False) = 0.0, Repeat: Int = 2) -> [Number]:
     return np.repeat(Values, Repeat)
 
 
 @node_func(id=1)
-def np_tile(Values: Number = 0.0, Repeat: Int = 2) -> Number:
+@generator
+def np_tile(Values: Number(iterable=False) = 0.0, Repeat: Int = 2) -> [Number]:
     return np.tile(Values, Repeat)
-
-
