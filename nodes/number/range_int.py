a77d86ae999b31880f2e1f4ddc2e301387d78afc
zeffii <tetha.z@gmail.com>
2017-02-22 21:50:48 +0100

    range count shell started

--- nodes/number/range_int.py
+++ nodes/number/range_int.py
@@ -1,14 +1,18 @@
 import numpy as np
 
 from svrx.nodes.node_base import node_func
-from svrx.typing import Float, Int
-from svrx.util.geom import generator
+from svrx.nodes.classes import NodeMathBase
 
+from svrx.typing import Int
 
+# pylint: disable=C0326
+# pylint: disable=W0622
+# pylint: disable=W0621
 
-@node_func(bl_idname="SvRxNumberRangeInt")
-@generator
-def arange(start: Int = 0,
-           stop: Int = 10,
-           step: Int = 1) -> [Int]:
-    return np.arange(start, stop, step)
+@node_func(bl_idname='SvRxNumberRangeInt', multi_label="Range", id=0, cls_bases=(NodeMathBase,))
+def range(start: Int = 0, step: Int = 1, stop: Int = 10) -> [Int]:
+    return np.arange(start, step, stop)
+
+@node_func(id=1)
+def count(start: Int = 0, step: Int = 1, count: Int = 10) -> [Int]:
+    return np.arange(start, step, count)
