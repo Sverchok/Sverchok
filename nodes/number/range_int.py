05aafab4e7e827abe9b654c12c1bd613aad77704
zeffii <tetha.z@gmail.com>
2017-02-23 10:27:47 +0100

    quick version, no min.max tests 2

--- nodes/number/range_int.py
+++ nodes/number/range_int.py
@@ -1,18 +1,19 @@
 import numpy as np
 
 from svrx.nodes.node_base import node_func
-from svrx.nodes.classes import NodeMathBase
-
-from svrx.typing import Int
+from svrx.util.geom import generator
+from svrx.typing import Int, Bool
 
 # pylint: disable=C0326
 # pylint: disable=W0622
 # pylint: disable=W0621
 
-@node_func(bl_idname='SvRxNumberRangeInt', multi_label="Range", id=0, cls_bases=(NodeMathBase,))
+@node_func(bl_idname='SvRxNumberRangeInt', multi_label="Range", id=0)
+@generator
 def range(start: Int = 0, step: Int = 1, stop: Int = 10) -> [Int]:
-    return np.arange(start, step, stop)
+    return np.arange(start, np.copysign(step, stop), stop)
 
 @node_func(id=1)
+@generator
 def count(start: Int = 0, step: Int = 1, count: Int = 10) -> [Int]:
-    return np.arange(start, count*step, step)
+    return np.arange(start, np.copysign(count*step, step), step)
