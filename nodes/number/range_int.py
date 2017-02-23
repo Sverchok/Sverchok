b9f759249279a27521735f07cb78ff3d04b47cd0
zeffii <tetha.z@gmail.com>
2017-02-23 12:20:32 +0100

    count is almost ok

--- nodes/number/range_int.py
+++ nodes/number/range_int.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from svrx.nodes.node_base import node_func
 from svrx.util.geom import generator
-from svrx.typing import Int, Bool
+from svrx.typing import Int
 
 # pylint: disable=C0326
 # pylint: disable=W0622
@@ -11,9 +11,10 @@ from svrx.typing import Int, Bool
 @node_func(bl_idname='SvRxNumberRangeInt', multi_label="Range Int", id=0)
 @generator
 def range(start: Int = 0, step: Int = 1, stop: Int = 10) -> [Int]:
-    return np.arange(start, stop, np.copysign(step, stop))
+    final_step = np.copysign(step, stop)
+    return np.arange(start, stop + final_step, final_step)
 
 @node_func(id=1)
 @generator
 def count(start: Int = 0, step: Int = 1, count: Int = 10) -> [Int]:
-    return np.arange(start, np.copysign(count*step, step), step)
+    return np.arange(start, np.copysign(count * step, step), step)
